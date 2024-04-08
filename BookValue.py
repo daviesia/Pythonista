934883b78ac84d10cf5c9fe38a87fe583075179a
Ian D. Davies <iddavies@sympatico.ca>
2024-04-08 12:57:38 -0400

    Cleaned up the code

--- BookValue.py
+++ BookValue.py
@@ -10,11 +10,10 @@ with open('Data/transactions_summary.csv', mode='r') as csv_file:
     book_value = 0
     for row in csv_reader:
         if line_count == 0:
-            print(f'Column names are {", ".join(row)}')
+            # print(f'Column names are {", ".join(row)}')
             line_count += 1
-        # print(f'\t{row["name"]} works in the {row["department"]} department, and was born in {row["birthday month"]}.')
+   
         if row["Transaction Type"] == "purchase/sale":
-            # print(f'\t{row["Transaction Type"]}')
             line_count += 1
             book_value += int(row["Amount Debited"])
     print(f'\nProcessed {line_count} lines.')
